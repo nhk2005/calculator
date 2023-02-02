8d4bdf288ed2d502f38288aef6527825a55140a2
nhk2005 <nhk2005.personal@gmail.com>
2023-02-02 18:01:53 +0700

    Add functionality to compute simple calculations

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
-let operand = Array(2), operator;
+let operand = Array(2), operator, result;
 let operandIndex;
 
 function showEquation() {
   document.querySelector(".equation").textContent = `${operand[0]} ${operator} ${operand[1]}`;
+}
+
+function showResult() {
+  document.querySelector(".result").textContent = result;
 }
 
 function clear() {
@@ -30,6 +34,26 @@ function pressOperator(newOperator) {
   operandIndex = 1;
 }
 
+function computeResult() {
+  if (operand[0] === "" || operand[1] === "" || operator === "") {
+    return;
+  }
+
+  const number1 = Number(operand[0]);
+  const number2 = Number(operand[1]);
+  if (operator === "+") {
+    result = number1 + number2;
+  } else if (operator === "-") {
+    result = number1 - number2;
+  } else if (operator === "x") {
+    result = number1 * number2;
+  } else {
+    result = number1 / number2;
+  }
+
+  showResult();
+}
+
 clear();
 
 document.querySelector(".clear").addEventListener("click", () => clear());
@@ -41,3 +65,5 @@ document.querySelectorAll(".digit").forEach((digitButton) => {
 document.querySelectorAll(".operator").forEach((operatorButton) => {
   operatorButton.addEventListener("click", () => pressOperator(operatorButton.textContent));
 });
+
+document.querySelector(".equal").addEventListener("click", () => computeResult());
